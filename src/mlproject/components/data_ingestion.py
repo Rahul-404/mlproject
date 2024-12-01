10301800d69cdabd81cfac110e1bdef5263b38b9
Rahul-404 <rahulshelke981@gmail.com>
2024-12-01 19:01:39 +0530

    completed with models training

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -8,6 +8,12 @@ from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass
 
+from src.mlproject.components.data_transfromation import DataTransformation
+from src.mlproject.components.data_transfromation import DataTransformationConfig
+
+from src.mlproject.components.model_trainer import ModelTrainerConfig
+from src.mlproject.components.model_trainer import ModelTrainer
+
 # data class 
 @dataclass
 class DataIngestionConfig:
@@ -41,3 +47,16 @@ class DataIngestion:
 
         except Exception as e:
             raise CustomeException(e, sys)
+        
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
+
