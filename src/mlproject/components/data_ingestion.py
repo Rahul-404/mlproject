f47a7ade8dd5c19d970fe403941d5bfaf26a4062
Rahul-404 <rahulshelke981@gmail.com>
2024-12-02 01:30:09 +0530

    commented test code

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -48,15 +48,15 @@ class DataIngestion:
         except Exception as e:
             raise CustomeException(e, sys)
         
+# code for testing purpose
+# if __name__ == "__main__":
+#     obj = DataIngestion()
+#     train_data, test_data = obj.initiate_data_ingestion()
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
+#     data_transformation = DataTransformation()
+#     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
-
-    model_trainer = ModelTrainer()
-    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+#     model_trainer = ModelTrainer()
+#     print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
 
